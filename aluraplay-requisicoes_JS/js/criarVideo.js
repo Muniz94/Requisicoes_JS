98232b6eff101f80a0d6988029e68b4ab0203d35
muniz94 <muniz91@hotmail.com>
2023-11-01 13:28:58 -0300

    Gerei um número aleatório para a descrição

--- aluraplay-requisicoes_JS/js/criarVideo.js
+++ aluraplay-requisicoes_JS/js/criarVideo.js
@@ -4,4 +4,5 @@ function criaVideo() {
     const imagem = document.querySelector("[data-imagem]").value;
     const url = document.querySelector("[data-url]").value;
     const titulo = document.querySelector("[data-titulo]").value;
+    const descricao = Math.floor(Math.random() * 10).toString();
 }
