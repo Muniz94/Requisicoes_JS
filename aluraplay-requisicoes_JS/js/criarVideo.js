3c2681976288b36cae3716303bcb80ca10186225
muniz94 <muniz91@hotmail.com>
2023-11-01 13:26:41 -0300

    Função 'criaVideo', as const recebem o valor digitado pelo usuário, não o elemento inteiro

--- aluraplay-requisicoes_JS/js/criarVideo.js
+++ aluraplay-requisicoes_JS/js/criarVideo.js
@@ -1,2 +1,7 @@
 const formulario = document.querySelector("[data-formulario]");
 
+function criaVideo() {
+    const imagem = document.querySelector("[data-imagem]").value;
+    const url = document.querySelector("[data-url]").value;
+    const titulo = document.querySelector("[data-titulo]").value;
+}
