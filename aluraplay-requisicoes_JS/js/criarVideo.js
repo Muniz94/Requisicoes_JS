7fbacbb4b65bddd4a0b60155307efded70a8d6a8
muniz94 <muniz91@hotmail.com>
2023-11-02 17:24:02 -0300

    Usei o 'async/await'
    Como a função 'criaVideo' é assíncrona e retorna uma promise, tive que transformar essa outra função em assíncrona também para que a função seja acessada mesmo antes da promise ser resolvida

--- aluraplay-requisicoes_JS/js/criarVideo.js
+++ aluraplay-requisicoes_JS/js/criarVideo.js
@@ -1,7 +1,7 @@
 import { conectaAPI } from "./conectaAPI.js";
 const formulario = document.querySelector("[data-formulario]");
 
-function criaVideo(evento) {
+async function criaVideo(evento) {
     evento.preventDefault();
     
     const imagem = document.querySelector("[data-imagem]").value;
@@ -9,7 +9,7 @@ function criaVideo(evento) {
     const titulo = document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 10).toString();
 
-    conectaAPI.criaVideo(titulo, descricao, url, imagem);
+    await conectaAPI.criaVideo(titulo, descricao, url, imagem);
 }
 
 formulario.addEventListener("submit", evento => criaVideo(evento));
