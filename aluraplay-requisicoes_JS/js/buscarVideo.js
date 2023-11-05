994606745f594d926f62107b2cdfa426b98595f8
Lucas Muniz <muniz@hotmail.com>
2023-11-05 16:03:02 -0300

    Adicionei um EventListener ao 'click' do 'botaoDePesquisa'

--- aluraplay-requisicoes_JS/js/buscarVideo.js
+++ aluraplay-requisicoes_JS/js/buscarVideo.js
@@ -1,9 +1,10 @@
 import { conectaAPI } from "./conectaAPI.js";
 
-async function buscarVideo() {
+async function buscarVideo(evento) {
   const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
   const busca = await conectaAPI.buscaVideo(dadosDePesquisa);
 
   const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]"); 
 
+  botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento))
 }
