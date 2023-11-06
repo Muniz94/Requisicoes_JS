d980f8c8cb75a51810283a27918ea283304da9af
muniz94 <muniz91@hotmail.com>
2023-11-06 13:01:24 -0300

    Criei um 'forEach' (elemento da busca anexa um filho na 'lista')

--- aluraplay-requisicoes_JS/js/buscarVideo.js
+++ aluraplay-requisicoes_JS/js/buscarVideo.js
@@ -8,6 +8,9 @@ async function buscarVideo(evento) {
   const busca = await conectaAPI.buscaVideo(dadosDePesquisa);
 
   const lista = document.querySelector("[data-lista]");
+
+  busca.forEach(element => lista.appendChild(
+    constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]"); 
